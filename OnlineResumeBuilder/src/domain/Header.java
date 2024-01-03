857ff267d2bfca2c5084cd6a1538d0b0ae2f25fa
vishal <vishal88kh@gmail.com>
2024-01-04 00:58:17 +0530

    added instances for Header class

--- OnlineResumeBuilder/src/domain/Header.java
+++ OnlineResumeBuilder/src/domain/Header.java
@@ -1,4 +1,11 @@
 package domain;
 
 public class Header {
+    private String fullName;
+    private String designation;
+    private String emailId;
+    private String contactNumber;
+    private String address;
+    private String DOB;
+
 }
