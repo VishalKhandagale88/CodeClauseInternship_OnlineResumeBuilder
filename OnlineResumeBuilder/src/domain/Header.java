0e49a085c398b2e8d00cae85f4cb5a8d280c6634
vishal <vishal88kh@gmail.com>
2024-01-04 00:59:45 +0530

    added constructor

--- OnlineResumeBuilder/src/domain/Header.java
+++ OnlineResumeBuilder/src/domain/Header.java
@@ -8,4 +8,14 @@ public class Header {
     private String address;
     private String DOB;
 
+    public Header(String fullName, String designation, String emailId, String contactNumber, String address, String DOB) {
+        this.fullName = fullName;
+        this.designation = designation;
+        this.emailId = emailId;
+        this.contactNumber = contactNumber;
+        this.address = address;
+        this.DOB = DOB;
+    }
+
+
 }
