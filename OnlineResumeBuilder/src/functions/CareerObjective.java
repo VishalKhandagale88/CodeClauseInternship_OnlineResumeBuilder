6fe03259adedd6ab9374900a9f4de700b744b9b2
vishal <vishal88kh@gmail.com>
2024-01-04 23:38:18 +0530

    added CareerObjective pojo class`

--- OnlineResumeBuilder/src/functions/CareerObjective.java
+++ OnlineResumeBuilder/src/functions/CareerObjective.java
@@ -1,5 +1,43 @@
 package functions;
 
+import java.util.Objects;
+
 public class CareerObjective {
     private String description;
+
+    public CareerObjective(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return "About Me";
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CareerObjective that = (CareerObjective) o;
+        return Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description);
+    }
+
+    @Override
+    public String toString() {
+        return "CareerObjective{" +
+                "description='" + description + '\'' +
+                '}';
+    }
 }
