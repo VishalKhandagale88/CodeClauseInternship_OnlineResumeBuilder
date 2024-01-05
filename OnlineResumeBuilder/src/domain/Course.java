6f16e844b06cdd89df0f9720a4f6b24e8cbc414b
vishal <vishal88kh@gmail.com>
2024-01-05 16:54:04 +0530

    added Course class

---
+++ OnlineResumeBuilder/src/domain/Course.java
@@ -0,0 +1,50 @@
+package domain;
+
+import java.util.Objects;
+
+public class Course {
+    private String name;
+    private String description;
+
+    public Course(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(name, course.name) && Objects.equals(description, course.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
