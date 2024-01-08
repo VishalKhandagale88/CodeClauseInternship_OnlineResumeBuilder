a35407aac452f398a372b1ae2c3ea5ffe8673c43
vishal <vishal88kh@gmail.com>
2024-01-08 23:22:41 +0530

    added Education class

--- OnlineResumeBuilder/src/Main.java
+++ OnlineResumeBuilder/src/Main.java
@@ -10,10 +10,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // all objects
-//        Header headerFromUserInput = HeaderFunctionality.createHeaderFromUserInput();
-//        System.out.println(headerFromUserInput);
-//        CareerObjective careerObjective = CareerObjectiveFunctionality.careerObjectiveWindow();
-//        System.out.println(careerObjective);
+        Header headerFromUserInput = HeaderFunctionality.createHeaderFromUserInput();
+        System.out.println(headerFromUserInput);
+        CareerObjective careerObjective = CareerObjectiveFunctionality.careerObjectiveWindow();
+        System.out.println(careerObjective);
 
         List<Course> courses = CourseFunctionality.CoursesWindow();
         System.out.println(courses);
