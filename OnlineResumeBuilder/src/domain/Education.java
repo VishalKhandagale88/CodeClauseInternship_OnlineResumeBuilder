5fe235a81afd1e6733712b605d3a87515c805771
vishal <vishal88kh@gmail.com>
2024-01-08 23:24:34 +0530

    added instanced for Eduction class

--- OnlineResumeBuilder/src/domain/Education.java
+++ OnlineResumeBuilder/src/domain/Education.java
@@ -1,4 +1,9 @@
 package domain;
 
 public class Education {
+    private String courseName;
+    private String instituteName;
+    private String percentile;
+    private String startYear;
+    private String endYear;
 }
