2b14555bb08ec283daa97c20e94311b59bf93a6d
vishal <vishal88kh@gmail.com>
2024-01-11 00:51:47 +0530

    added instances

---
+++ OnlineResumeBuilder/src/domain/Experience.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Experience {
+    private String companyName;
+    private String role;
+    private String description;
+    private String startYear;
+    private String endYear;
+
+}
