cef466e426741bc22e110c98560004daba71288f
vishal <vishal88kh@gmail.com>
2024-01-10 17:18:20 +0530

    added toString method

--- OnlineResumeBuilder/src/domain/Project.java
+++ OnlineResumeBuilder/src/domain/Project.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Project {
     private String title;
     private String about;
@@ -45,4 +47,26 @@ public class Project {
         this.end = end;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Project project = (Project) o;
+        return Objects.equals(title, project.title) && Objects.equals(about, project.about) && Objects.equals(star, project.star) && Objects.equals(end, project.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, about, star, end);
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "title='" + title + '\'' +
+                ", about='" + about + '\'' +
+                ", star='" + star + '\'' +
+                ", end='" + end + '\'' +
+                '}';
+    }
 }
