cb84fc2fbfbd886c73babda6dc9badb1f064d610
vishal <vishal88kh@gmail.com>
2024-01-10 17:17:23 +0530

    added constructor

--- OnlineResumeBuilder/src/domain/Project.java
+++ OnlineResumeBuilder/src/domain/Project.java
@@ -6,4 +6,11 @@ public class Project {
     private String star;
     private String end;
 
+    public Project(String title, String about, String star, String end) {
+        this.title = title;
+        this.about = about;
+        this.star = star;
+        this.end = end;
+    }
+
 }
