842a7144b4e4ad932e1f5e4b9e748446beb0c0d0
vishal <vishal88kh@gmail.com>
2024-01-11 00:52:19 +0530

    added constructor

--- OnlineResumeBuilder/src/domain/Experience.java
+++ OnlineResumeBuilder/src/domain/Experience.java
@@ -7,4 +7,12 @@ public class Experience {
     private String startYear;
     private String endYear;
 
+    public Experience(String companyName, String role, String description, String startYear, String endYear) {
+        this.companyName = companyName;
+        this.role = role;
+        this.description = description;
+        this.startYear = startYear;
+        this.endYear = endYear;
+    }
+
 }
