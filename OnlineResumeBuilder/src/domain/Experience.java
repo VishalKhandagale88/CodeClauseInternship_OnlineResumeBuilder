f63e824bcd7819b83dc731fd9c08b9948badf0ae
vishal <vishal88kh@gmail.com>
2024-01-11 00:52:42 +0530

    added getters and setters

--- OnlineResumeBuilder/src/domain/Experience.java
+++ OnlineResumeBuilder/src/domain/Experience.java
@@ -15,4 +15,43 @@ public class Experience {
         this.endYear = endYear;
     }
 
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(String startYear) {
+        this.startYear = startYear;
+    }
+
+    public String getEndYear() {
+        return endYear;
+    }
+
+    public void setEndYear(String endYear) {
+        this.endYear = endYear;
+    }
 }
