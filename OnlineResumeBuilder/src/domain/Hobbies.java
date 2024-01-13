413084a68cca6add19669a0dd6ca29ba0e3550b4
vishal <vishal88kh@gmail.com>
2024-01-13 17:52:20 +0530

    added constructor and getter setter

--- OnlineResumeBuilder/src/domain/Hobbies.java
+++ OnlineResumeBuilder/src/domain/Hobbies.java
@@ -1,4 +1,17 @@
 package domain;
 
 public class Hobbies {
+    private String name;
+
+    public Hobbies(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
