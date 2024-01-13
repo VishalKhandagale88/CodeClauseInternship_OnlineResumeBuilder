341d8d2d648d38357c7dbdc25ede98a3ffcd2603
vishal <vishal88kh@gmail.com>
2024-01-13 17:52:51 +0530

    added toString() method

--- OnlineResumeBuilder/src/domain/Hobbies.java
+++ OnlineResumeBuilder/src/domain/Hobbies.java
@@ -14,4 +14,11 @@ public class Hobbies {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Hobbies{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
